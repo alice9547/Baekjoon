684fcc07544b8b15b4cf572bd43bd4409a4fbf8e
EunSeo <alice9547@gmail.com>
2023-11-05 18:15:06 +0900

    17298추가

---
+++ codingTest.playground/Sources/Q17298.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+import Foundation
+
+let N = Int(readLine()!)!
+let A = readLine()!.split(separator: " ").map { Int($0)! }
+
+var result = [Int](repeating: -1, count: N)
+var stack = [Int]()
+
+private func method17298() {
+    for i in 0..<N {
+        while !stack.isEmpty && A[i] > A[stack.last!] {
+            let index = stack.removeLast()
+            result[index] = A[i]
+        }
+        stack.append(i)
+    }
+    
+    print(result.map { String($0) }.joined(separator: " "))
+}
