accc29e4820f3be6954c273d2c08be9fa37ddc70
EunSeo <alice9547@gmail.com>
2023-11-12 19:34:04 +0900

    Q10809 추가

--- codingTest.playground/Contents.swift
+++ codingTest.playground/Contents.swift
@@ -1,11 +1,15 @@
 import Foundation
 
-let input = "baekjoon"
-var stack = [Int](repeating: 0, count: 26)
+let input = Array("abca")
+var stack = [Int](repeating: -1, count: 26)
 
-for i in input {
-    let index = i.asciiValue! % 97
-    stack[Int(index)] += 1
+for i in 0..<input.count {
+    let index = input[i].asciiValue! % 97
+    
+    if stack[Int(index)] == -1 {
+        stack[Int(index)] = i
+    }
 }
+
 let result = stack.map {String($0)}
 print(result.joined(separator: " "))
