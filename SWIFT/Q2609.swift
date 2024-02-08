95c6018687c2dd8c2cc97bd7e844642dca1cfe08
김은서 <alice9547@naver.com>
2024-02-08 16:21:41 +0900

    Q2609

--- SWIFT/Q2609.swift
+++ SWIFT/Q2609.swift
@@ -1,16 +1,8 @@
-var nums = readLine()!.split(separator: " ").map {Int($0)!}
-
-func gcd(_ a: Int, _ b: Int) -> Int {
-    if b == 0 {
-        return a
-    } else {
-        return gcd(b, a % b)
-    }
+func gcd(_ a: Int, _ b: Int) {
+    return b == 0 ? a : gcd(b, a%b)
 }
 
-func lcm(_ a: Int, _ b: Int) -> Int {
-    return a * b / gcd(a, b)
+for _ in 0..<Int(readLine()!) {
+    var nums = readLine()!.split(separator: " ").map {}
+    print(nums[0] * nums[1] / gcd(nums[0], nums[1]))
 }
-
-print(gcd(nums[0], nums(1)))
-print(lcm(nums[0], nums(1)))
