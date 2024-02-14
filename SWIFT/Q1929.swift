af9d38081c841531c1f50e4e46da73ef246f38ea
김은서 <alice9547@naver.com>
2024-02-14 10:33:35 +0900

    1929 에라토스체네스의 체 이용

--- SWIFT/Q1929.swift
+++ SWIFT/Q1929.swift
@@ -1,17 +1,19 @@
 let nums = readLine()!.split(separator: " ").map {Int($0)!}
 let N = nums[1]
 
-var result: [Int] = Array(repeating: 0, count: N + 1)
-for i in 2...N {
-    result[i] = i
-}
+var result: [Bool] = Array(repeating: true, count: N + 1)
+result[1] = false
 
 for i in 2...nums[1] {
-    if result[i] == 0 { continue }
-    for k in stride(from: i+i, through: N, by: i) {
-        result[k] = 0
+    if result[i] {
+        for k in stride(from: i+i, through: N, by: i) {
+            result[k] = false
+        }
     }
 }
-result.filter({$0 != 0}).forEach {
-    print($0)
-}
+
+result.forEach({ index, value in
+    if value {
+        print(index)
+    }
+})
