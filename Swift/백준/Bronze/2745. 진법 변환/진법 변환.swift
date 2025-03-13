bed7c3f3f0f7acc5cafd388a1f8530e3e456e063
KES <73085209+alice9547@users.noreply.github.com>
2025-03-13 13:38:52 +0900

    [Bronze II] Title: 진법 변환, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- Swift/백준/Bronze/2745. 진법 변환/진법 변환.swift
+++ Swift/백준/Bronze/2745. 진법 변환/진법 변환.swift
@@ -1,13 +1,2 @@
-import Foundation
-
 var nums = readLine()!.split(separator: " ")
-var result = 0
-
-nums[0].reversed().enumerated().forEach({
-    var value: Double = Double($1.asciiValue!)
-    value = value >= 65 ? value - 55 : value - 48
-    let a: Double = pow(Double(nums[1])!, Double($0)) * value
-    result += Int(a)
-})
-
-print(result)
+print(Int(nums[0], radix: Int(nums[1])!)!)
