b1dcf488f6cb3f8c467ecec98145ec07c7d75b37
KES <73085209+alice9547@users.noreply.github.com>
2025-03-21 08:29:57 +0900

    [Gold V] Title: 합분해, Time: 8 ms, Memory: 69368 KB -BaekjoonHub

--- Swift/백준/Gold/2225. 합분해/합분해.swift
+++ Swift/백준/Gold/2225. 합분해/합분해.swift
@@ -3,8 +3,8 @@ let N = nk[0]
 let K = nk[1]
 var dp = [[Int]](repeating: [Int](repeating: 0, count: N+1), count: K+1)
 
-for i in 1..<K+1 {
-    for j in 0..<N+1 {
+for i in 1...K {
+    for j in 0...N {
         if j == 0 {
             dp[i][0] = 1
         } else {
