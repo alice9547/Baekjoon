cfd482f2edde3ec275468563424e34d7b9f2d819
KES <73085209+alice9547@users.noreply.github.com>
2025-03-21 13:55:37 +0900

    [Silver I] Title: 동물원, Time: 8 ms, Memory: 69884 KB -BaekjoonHub

--- Swift/백준/Silver/1309. 동물원/동물원.swift
+++ Swift/백준/Silver/1309. 동물원/동물원.swift
@@ -1,11 +1,8 @@
-let N = Int(readLine()!)!
-let mod = 9901
-var dp = [[Int]](repeating: [0,0,0], count: N+2)
-dp[1] = [1,1,1]
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: n+1)
+dp[0...1] = [1,3]
 
-for i in 2..<N+1 {
-    dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % mod
-    dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % mod
-    dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % mod
+for i in 2..<n+1 {
+    dp[i] = ((2*dp[i-1]) + dp[i-2]) % 9901
 }
-print((dp[N].reduce(0){$0+$1}) % mod)
+print(dp[n])
