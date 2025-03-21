447fbecdc45c0d5e22940aed9273620cf28ca9f1
KES <73085209+alice9547@users.noreply.github.com>
2025-03-21 11:44:05 +0900

    [Silver I] Title: RGB거리, Time: 8 ms, Memory: 69240 KB -BaekjoonHub

---
+++ Swift/백준/Silver/1149. RGB거리/RGB거리.swift
@@ -0,0 +1,14 @@
+let N = Int(readLine()!)!
+var cost = [[Int]]()
+for _ in 0..<N {
+    cost.append(readLine()!.split(separator: " ").map {Int($0)!})
+}
+var dp = [[Int]](repeating: [0,0,0], count: N)
+dp[0] = cost[0]
+
+for i in 1..<N {
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + cost[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + cost[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + cost[i][2]
+}
+print(dp[N-1].min()!)
