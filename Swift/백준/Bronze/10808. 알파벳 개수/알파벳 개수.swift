ab73bf87a7066507df055e5b2893d2036613041d
KES <73085209+alice9547@users.noreply.github.com>
2025-03-21 16:54:16 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ Swift/백준/Bronze/10808. 알파벳 개수/알파벳 개수.swift
@@ -0,0 +1,9 @@
+let input = readLine()!
+var stack = [Int](repeating: 0, count: 26)
+
+for i in input {
+    let index = i.asciiValue! % 97
+    stack[Int(index)] += 1
+}
+let result = stack.map {String($0)}
+print(result.joined(separator: " "))
