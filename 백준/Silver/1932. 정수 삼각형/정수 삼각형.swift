bf035c35b9dce58af6290f983d7d105ea529ab17
KES <73085209+alice9547@users.noreply.github.com>
2025-03-23 16:24:17 +0900

    [Silver I] Title: 정수 삼각형, Time: 44 ms, Memory: 72160 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.swift
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.swift
@@ -4,15 +4,11 @@ for _ in 0..<n {
     list.append(readLine()!.split(separator: " ").map {Int($0)!})
 }
 var dp = [[Int]](repeating: [Int](repeating: 0, count: n), count: n+1)
-dp[0][0] = list[0][0]
 
 for i in 1...n {
-    for j in 0..<list[i].count {
-        if j == 0 {
-            dp[i][j] = dp[i-1][0] + list[i][0]
-        } else {
-            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + list[i][j]
-        }
+    dp[i][0] = dp[i-1][0] + list[i][0]
+    for j in 1..<list[i].count {
+        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + list[i][j]
     }
 }
 print(dp[n].max()!)
