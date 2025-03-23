bc9dce8263ea9ea02d3a412d2e63a174d6ff82d9
KES <73085209+alice9547@users.noreply.github.com>
2025-03-23 15:21:12 +0900

    [Silver I] Title: 포도주 시식, Time: 12 ms, Memory: 69236 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.swift
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.swift
@@ -4,15 +4,14 @@ for _ in 0..<n {
     list.append(Int(readLine()!)!)
 }
 var dp = [Int](repeating: 0, count: n+1)
+dp[1] = list[1]
 
-if n == 1 {
-    print(list[n])
-} else if n == 2 {
-    print(list[1]+list[2])
-} else {
-    dp[1...2] = [list[1], list[1]+list[2]]
+if n > 1 {
+    dp[2] = list[1]+list[2]
+}
+if n > 2 {
     for i in 3...n {
         dp[i] = max(dp[i-1], dp[i-2]+list[i], dp[i-3]+list[i-1]+list[i])
     }
-    print(dp[n])
 }
+print(dp[n])
