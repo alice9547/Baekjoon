5d444bf19eb7735c16563095b1075381bf9454b3
KES <73085209+alice9547@users.noreply.github.com>
2025-04-08 16:01:41 +0900

    [Silver II] Title: N과 M (10), Time: 8 ms, Memory: 69116 KB -BaekjoonHub

---
+++ Swift/백준/Silver/15664. N과 M （10）/N과 M （10）.swift
@@ -0,0 +1,29 @@
+let input = readLine()!.split(separator: " ").map {Int($0)!}
+let N = input[0], M = input[1]
+let list = readLine()!.split(separator: " ").map {Int($0)!}.sorted()
+
+var visit = Array(repeating: false, count: N)
+var result = [String]()
+var check = Set<String>()
+
+func dfs(_ prev: Int) {
+    if result.count == M {
+        let resultString = result.joined(separator: " ")
+        if !check.contains(resultString) {
+            check.insert(resultString)
+            print(resultString)
+        }
+        return
+    }
+    
+    for i in 0..<N {
+        if !visit[i] && prev <= list[i] {
+            visit[i] = true
+            result.append("\(list[i])")
+            dfs(list[i])
+            visit[i] = false
+            result.popLast()
+        }
+    }
+}
+dfs(0)
