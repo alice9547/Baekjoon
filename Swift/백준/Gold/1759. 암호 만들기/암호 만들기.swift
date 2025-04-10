d4183d64976a20aaff421b0fda73e932700aaccc
KES <73085209+alice9547@users.noreply.github.com>
2025-04-10 12:01:39 +0900

    [Gold V] Title: 암호 만들기, Time: 8 ms, Memory: 69112 KB -BaekjoonHub

--- Swift/백준/Gold/1759. 암호 만들기/암호 만들기.swift
+++ Swift/백준/Gold/1759. 암호 만들기/암호 만들기.swift
@@ -1,5 +1,5 @@
-let LC = readLine()!.split(separator: " ").map { Int($0)! }
-let list = readLine()!.split(separator: " ").map { String($0) }.sorted()
+let LC = readLine()!.split(separator: " ").map {Int($0)!}
+let list = readLine()!.split(separator: " ").map {String($0)}.sorted()
 
 let L = LC[0]
 let C = LC[1]
@@ -9,22 +9,19 @@ var output = ""
 func dfs(_ str: String, _ start: Int, _ depth: Int) {
     if depth == L {
         var cntVo = 0
-        var cntCo = 0
         for char in str {
             if vo.contains(String(char)) {
                 cntVo += 1
-            } else {
-                cntCo += 1
             }
         }
-        if cntVo >= 1 && cntCo >= 2 {
+        
+        if cntVo >= 1 && L-cntVo >= 2 {
             output += str + "\n"
         }
-        return
     }
-
+    
     for i in start..<C {
-        dfs(str + list[i], i + 1, depth + 1)
+        dfs(str+list[i], i+1, depth+1)
     }
 }
 
