d66b7d0e8c75427e721aa60acdb324dc485c20d2
KES <73085209+alice9547@users.noreply.github.com>
2025-04-13 19:41:34 +0900

    [Gold V] Title: 링크와 스타트, Time: 1184 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.swift
@@ -0,0 +1,38 @@
+let N = Int(readLine()!)!
+var S = [[Int]]()
+for _ in 0..<N {
+    S.append(readLine()!.split(separator: " ").map {Int($0)!})
+}
+var visit = Array(repeating: false, count: N)
+var result = Int.max
+
+func dfs(_ start: Int, _ depth: Int) {
+    if depth > 1 {
+        var team1 = 0
+        var team2 = 0
+        
+        for i in 0..<N {
+            for j in 0..<N {
+                if visit[i], visit[j] {
+                    team1 += S[i][j]
+                }
+                if !visit[i], !visit[j] {
+                    team2 += S[i][j]
+                }
+            }
+        }
+        result = min(result, abs(team1-team2))
+    }
+    
+    if depth == N { return }
+    
+    for i in start..<N {
+        if !visit[i] {
+            visit[i] = true
+            dfs(i, depth+1)
+            visit[i] = false
+        }
+    }
+}
+dfs(0, 0)
+print(result)
