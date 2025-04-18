20d37adc7cd5e2b96132d55ec55b94ab6735a112
KES <73085209+alice9547@users.noreply.github.com>
2025-04-18 12:08:00 +0900

    [Silver I] Title: 단지번호붙이기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- Swift/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
+++ Swift/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.swift
@@ -4,31 +4,27 @@ for _ in 0..<N {
     list.append(readLine()!.map {Int(String($0))!})
 }
 var result = [Int]()
-var visit = [[Bool]](repeating: [Bool](repeating: false, count: N), count: N)
+var cnt = 0
 
-let dx = [-1,1,0,0]
-let dy = [0,0,-1,1]
-
-func dfs(_ x: Int, _ y: Int) -> Int {
-    visit[x][y] = true
-    var cnt = 1
-    for i in 0..<4 {
-        let nx = x+dx[i]
-        let ny = y+dy[i]
-        
-        if nx >= 0, nx < N, ny >= 0, ny < N {
-            if list[nx][ny] == 1 && !visit[nx][ny] {
-                cnt += dfs(nx, ny)
-            }
-        }
+func dfs(_ x: Int, _ y: Int) {
+    if x>=0, y>=0, x<N, y<N, list[x][y]==1 {
+        list[x][y] = 0
+        cnt += 1
+        dfs(x+1, y)
+        dfs(x-1, y)
+        dfs(x, y+1)
+        dfs(x, y-1)
+    } else {
+        return
     }
-    return cnt
 }
 
 for i in 0..<N {
     for j in 0..<N {
-        if list[i][j] == 1, !visit[i][j] {
-            result.append(dfs(i, j))
+        if list[i][j] == 1{
+            cnt = 0
+            dfs(i, j)
+            result.append(cnt)
         }
     }
 }
