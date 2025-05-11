d25b75dd47640caf9212105dd56144892d648942
KES <73085209+alice9547@users.noreply.github.com>
2025-05-11 17:42:21 +0900

    [Silver V] Title: 날짜 계산, Time: 4 ms, Memory: 69104 KB -BaekjoonHub

--- Swift/백준/Silver/1476. 날짜 계산/날짜 계산.swift
+++ Swift/백준/Silver/1476. 날짜 계산/날짜 계산.swift
@@ -1,11 +1,9 @@
-let nums = readLine()!.split(separator: " ").map {Int($0)!}
-let E = nums[0] == 15 ? 0 : nums[0]
-let S = nums[1] == 28 ? 0 : nums[1]
-let M = nums[2] == 19 ? 0 : nums[2]
+let input = readLine()!.split(separator: " ").map {Int($0)!}
+let E = input[0] % 15, S = input[1] % 28, M = input[2] % 19
 var result = 1
 
 while true {
-    if (result % 15 == E) && (result % 28 == S) && (result % 19 == M) {
+    if result % 15 == E, result % 28 == S, result % 19 == M {
         break
     } else {
         result += 1
