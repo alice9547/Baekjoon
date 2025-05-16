6d20a8cdba65f72e42cbe857b2b86897d4c32d49
KES <73085209+alice9547@users.noreply.github.com>
2025-05-16 14:41:55 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- Swift/백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.swift
+++ Swift/백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.swift
@@ -1,11 +1,6 @@
 let cmd = readLine()!.split(separator: " ").map {Int($0)!}
 let a = cmd[0], b = cmd[1], c = cmd[2], d = cmd[3], e = cmd[4], f = cmd[5]
 
-loop: for x in -999...999 {
-    for y in -999...999 {
-        if a*x + b*y == c, d*x + e*y == f {
-            print("\(x) \(y)")
-            break loop
-        }
-    }
-}
+let x = (e*c-b*f) / (a*e-b*d)
+let y = (a*f-d*c) / (a*e-b*d)
+print("\(x) \(y)")
