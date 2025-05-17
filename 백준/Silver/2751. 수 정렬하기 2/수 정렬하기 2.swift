709d6c028e705f7437efcf137a14796f57783def
KES <73085209+alice9547@users.noreply.github.com>
2025-05-17 17:17:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 688 ms, Memory: 105740 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.swift
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.swift
@@ -1,6 +1,8 @@
 let N = Int(readLine()!)!
 var list = [Int]()
+var result = ""
 for _ in 0..<N {
     list.append(Int(readLine()!)!)
 }
-list.sorted().forEach { print($0) }
+list.sorted().forEach { result += "\($0)\n" }
+print(result)
