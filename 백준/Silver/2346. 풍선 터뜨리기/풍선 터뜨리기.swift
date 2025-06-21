3251449d04b9ca5f453dbac0d4cbf3dcc596f9b0
KES <73085209+alice9547@users.noreply.github.com>
2025-06-21 17:41:27 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.swift
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.swift
@@ -1,15 +1,22 @@
-let n = Int(readLine()!)!
-var array = readLine()!.split { $0 == " " }.enumerated().map { (Int($0.element)!, $0.offset + 1) }
-while true {
-    let element = array.removeFirst()
-    let isMinus = element.0 < 0
-    print(element.1, terminator: " ")
-    if array.isEmpty { break }
-    let x = abs(element.0)
+let N = Int(readLine()!)!
+let paper = readLine()!.split(separator: " ").map { Int($0)! }
+var list = Array(1...N)
+var idx = 0
+var result = ""
+
+for _ in 0..<N {
+    let now = list[idx]
+    result += "\(now) "
+    let move = paper[now - 1]
+    list.remove(at: idx)
+
+    if list.isEmpty { break }
     
-    if !isMinus {
-        for _ in 0..<x - 1 { array.append(array.removeFirst()) }
-    } else {
-        for _ in 0..<x { array.insert(array.removeLast(), at: 0) }
+    idx = (idx + move + (move > 0 ? -1 : 0)) % list.count
+    
+    if idx < 0 {
+        idx += list.count
     }
 }
+
+print(result)
